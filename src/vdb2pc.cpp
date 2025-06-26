5fbb964f55a83787ffe10c5a398e2ed60329bc10
Lorenzo Ferrini <lorenzo.ferrini@pal-robotics.com>
2025-06-26 12:25:50 +0200

    it's linting over everything
    Trailing spaces vanish,
    Tabs and spaces reconciled—
    Code breathes peacefully.

--- src/vdb2pc.cpp
+++ src/vdb2pc.cpp
@@ -48,7 +48,7 @@
 
 // Explicit instantiations
 template void vdb2pc::transform<openvdb::Int32Grid>(
-    openvdb::Int32Grid& vdb_grid, pcl::PointCloud<pcl::PointXYZ>& cloud);
+  openvdb::Int32Grid & vdb_grid, pcl::PointCloud<pcl::PointXYZ> & cloud);
 
 template void vdb2pc::transform<openvdb::Int32Grid>(
-    openvdb::Int32Grid& vdb_grid, pcl::PointCloud<pcl::PointXYZI>& cloud);
+  openvdb::Int32Grid & vdb_grid, pcl::PointCloud<pcl::PointXYZI> & cloud);
